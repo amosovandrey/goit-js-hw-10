5dec76f90e91d1e8f8de8e27776d72d975a8ce7b
amosovandrey <rivercity.dp@gmail.com>
2023-06-17 18:56:00 +0300

    done

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,16 +1,31 @@
 const API_KEY =
-  'live_TxjcGQflbxvuSzHgmvblDnRxjRt8YKYIxaiKw6Fq1TnS0bFloCRxunVoMwmNkM0l';
+  'live_RmnFc4VeMGf3cxN1Oo0CiMQXRuIjI47DUP52eWTzCY7QBfozJJWlgEk8bSuD4lSU';
+
 const URL = 'https://api.thecatapi.com/v1/';
 
 const options = {
-  'x-api-key': API_KEY,
+  headers: {
+    'x-api-key': API_KEY,
+  },
 };
 
 export function fetchBreeds() {
-  return fetch(`${URL}breeds`, options).then(response => {
+  return fetch(`${URL}breeds?`, options).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return response.json;
+    return response.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${URL}images/search?breed_ids=${breedId}&has_breeds=1`,
+    options
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,75 @@
-import { fetchBreeds } from './cat-api';
+import Notiflix from 'notiflix';
+
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const selectEL = document.querySelector('.breed-select');
-console.dir(selectEL);
-selectEL.addEventListener('click', fetchBreeds);
-function addOption() {
-    const optionEl 
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+const cardEl = document.querySelector('.cat-info');
+
+hideElement(selectEL);
+hideElement(errorEl);
+
+fetchBreeds()
+  .then(breeds => {
+    showElement(selectEL);
+    renderOptionEl(breeds);
+    hideElement(loaderEl);
+    selectEL.addEventListener('change', renderCat);
+  })
+  .catch(error => showError(error));
+
+function renderOptionEl(breeds) {
+  const markup = breeds
+    .map(breed => {
+      return `
+          <option value='${breed.id}'>${breed.name}</option>
+      `;
+    })
+    .join('');
+
+  selectEL.innerHTML = markup;
+}
+
+function renderCat() {
+  showElement(loaderEl);
+  const breedId = selectEL.value;
+
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      const imageUrl = cat[0].url;
+      const breedName = cat[0].breeds[0].name;
+      const description = cat[0].breeds[0].description;
+      const temperament = cat[0].breeds[0].temperament;
+
+      const markup = `<h2>${breedName}</h2>
+        <p class="desc-text"><strong>Description:</strong> ${description}</p>
+        <p class="temper-text"> <strong>Temperament:</strong> ${temperament}</p>
+        <img src="${imageUrl}" width=320 alt="${breedName}" />
+      `;
+
+      cardEl.innerHTML = markup;
+      hideElement(loaderEl);
+    })
+    .catch(error => showError(error));
+}
+
+function showError(error) {
+  Notiflix.Notify.failure(
+    `Oops! 😮 This what's went wrong: ${error}! Try reloading the page!`,
+    {
+      position: 'center-top',
+      timeout: 4500,
+      pauseOnHover: true,
+      clickToClose: true,
+    }
+  );
+}
+
+function hideElement(element) {
+  element.classList.add('hidden');
+}
+
+function showElement(element) {
+  element.classList.remove('hidden');
 }
